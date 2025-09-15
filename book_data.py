32a01c6c83f0c0b3e2eba8a1950b31df4e615aea
Samiran2004 <samirans170@gmail.com>
2025-09-15 10:16:02 +0530

    update/Folder Structure

---
+++ book_data.py
@@ -0,0 +1,128 @@
+books = [
+  {
+    "id": 1,
+    "title": "Python Crash Course",
+    "author": "Eric Matthes",
+    "publisher": "No Starch Press",
+    "publish_date": "2019-05-03",
+    "page_count": 544,
+    "language": "English"
+  },
+  {
+    "id": 2,
+    "title": "Automate the Boring Stuff with Python",
+    "author": "Al Sweigart",
+    "publisher": "No Starch Press",
+    "publish_date": "2019-11-12",
+    "page_count": 592,
+    "language": "English"
+  },
+  {
+    "id": 3,
+    "title": "Python for Data Analysis",
+    "author": "Wes McKinney",
+    "publisher": "O'Reilly Media",
+    "publish_date": "2022-08-30",
+    "page_count": 579,
+    "language": "English"
+  },
+  {
+    "id": 4,
+    "title": "Learning Python",
+    "author": "Mark Lutz",
+    "publisher": "O'Reilly Media",
+    "publish_date": "2013-06-12",
+    "page_count": 1648,
+    "language": "English"
+  },
+  {
+    "id": 5,
+    "title": "Fluent Python",
+    "author": "Luciano Ramalho",
+    "publisher": "O'Reilly Media",
+    "publish_date": "2022-04-01",
+    "page_count": 1008,
+    "language": "English"
+  },
+  {
+    "id": 6,
+    "title": "Head First Python",
+    "author": "Paul Barry",
+    "publisher": "O'Reilly Media",
+    "publish_date": "2016-11-21",
+    "page_count": 624,
+    "language": "English"
+  },
+  {
+    "id": 7,
+    "title": "Python Cookbook",
+    "author": "David Beazley",
+    "publisher": "O'Reilly Media",
+    "publish_date": "2013-05-10",
+    "page_count": 706,
+    "language": "English"
+  },
+  {
+    "id": 8,
+    "title": "Learn Python the Hard Way",
+    "author": "Zed A. Shaw",
+    "publisher": "Addison-Wesley Professional",
+    "publish_date": "2017-06-26",
+    "page_count": 320,
+    "language": "English"
+  },
+  {
+    "id": 9,
+    "title": "Introduction to Machine Learning with Python",
+    "author": "Andreas C. Müller",
+    "publisher": "O'Reilly Media",
+    "publish_date": "2016-09-26",
+    "page_count": 400,
+    "language": "English"
+  },
+  {
+    "id": 10,
+    "title": "Effective Python",
+    "author": "Brett Slatkin",
+    "publisher": "Addison-Wesley Professional",
+    "publish_date": "2019-10-06",
+    "page_count": 352,
+    "language": "English"
+  },
+  {
+    "id": 11,
+    "title": "Python Tricks",
+    "author": "Dan Bader",
+    "publisher": "Real Python",
+    "publish_date": "2017-10-25",
+    "page_count": 301,
+    "language": "English"
+  },
+  {
+    "id": 12,
+    "title": "Hands-On Machine Learning",
+    "author": "Aurélien Géron",
+    "publisher": "O'Reilly Media",
+    "publish_date": "2022-10-15",
+    "page_count": 861,
+    "language": "English"
+  },
+  {
+    "id": 13,
+    "title": "The Quick Python Book",
+    "author": "Naomi Ceder",
+    "publisher": "Manning Publications",
+    "publish_date": "2018-05-15",
+    "page_count": 472,
+    "language": "English"
+  },
+  {
+    "id": 14,
+    "title": "Python Programming for Beginners",
+    "author": "Mark Reed",
+    "publisher": "CreateSpace Independent",
+    "publish_date": "2016-12-29",
+    "page_count": 194,
+    "language": "English"
+  }
+]
