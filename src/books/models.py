f3a798a342e74f45d01fa50e4bb40155ff22bb5e
Samiran2004 <samirans170@gmail.com>
2025-09-17 00:49:34 +0530

    add/Database Connection

---
+++ src/books/models.py
@@ -0,0 +1,28 @@
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+from datetime import datetime
+from uuid import UUID, uuid4
+
+class Book(SQLModel, table=True):
+
+    # __tablename__ is not needed with SQLModel, it is set automatically
+
+    uid: UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid4()
+        )
+    )
+    title: str
+    author: str
+    publisher: str
+    publish_date: str
+    page_count: int
+    language: str
+    created_at: datetime = Field(Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(Column(pg.TIMESTAMP, default=datetime.now))
+
+    def __repr__(self) -> str:
+        return f"<Book {self.title}>"
